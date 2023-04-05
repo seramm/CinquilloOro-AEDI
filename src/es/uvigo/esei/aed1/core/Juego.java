86123d7383d2a7259506208da4ebfa1111a845a4
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-04-05 20:45:39 +0200

    Llamada a pedirDatosJugadores()

--- src/es/uvigo/esei/aed1/core/Juego.java
+++ src/es/uvigo/esei/aed1/core/Juego.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 public class Juego{
   private final IU iu;
+  private List<Jugador> jugadores = new LinkedList<>();
     
     
 public Juego(IU iu){
@@ -19,31 +20,13 @@ public Juego(IU iu){
 
 }
 
-public List<Jugador> pedirDatosJugadores() {
-        int n;
-        List<Jugador> jugadores = new LinkedList<>();
-        do {
-            n = leeNum("Introduce número de jugadores: ");
-        } while (n < 3 && n > 4);
-
-        System.out.print("\n");
-
-        for (int i = 0; i < n; i++) {
-            String nombre;
-            nombre = leeString("Introduce nombre del jugador " + (i + 1) + ": ");
-            Jugador j = new Jugador(nombre);
-            jugadores.add(0, j);
-        }
-        return jugadores;
-    }
-
 public void jugar(){
 	//preguntar cuantos van a jugar
 	//crear los jugadores
 	//repartir las cartas entre los jugadores
 	//mostrar el estado de los jugadores
 	//indicar quien empieza la partida
-    
+   jugadores = iu.pedirDatosJugadores();
 }
 
         
