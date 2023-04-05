a505cc2a9f043e39788e5ef7b10ed0f10fe8fbb8
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-04-05 09:52:52 +0200

    Formatting

--- src/es/uvigo/esei/aed1/core/Juego.java
+++ src/es/uvigo/esei/aed1/core/Juego.java
@@ -1,31 +1,27 @@
 /**
- * Representa el juego del Cinquillo-Oro, con sus reglas (definidas en el documento Primera entrega). 
+ * Representa el juego del Cinquillo-Oro, con sus reglas (definidas en el documento Primera entrega).
  * Se recomienda una implementación modular.
  */
-
 package es.uvigo.esei.aed1.core;
 
 import es.uvigo.esei.aed1.iu.IU;
 
+public class Juego {
 
-public class Juego{
-  private final IU iu;
-    
-    
-public Juego(IU iu){
-    this.iu = iu;
+	private final IU iu;
 
-}
+	public Juego(IU iu) {
+		this.iu = iu;
 
-public void jugar(){
-	//preguntar cuantos van a jugar
-	//crear los jugadores
-	//repartir las cartas entre los jugadores
-	//mostrar el estado de los jugadores
-	//indicar quien empieza la partida
-    
-}
+	}
 
-        
-    
+	public void jugar() {
+		//preguntar cuantos van a jugar
+		//crear los jugadores
+		//repartir las cartas entre los jugadores
+		//mostrar el estado de los jugadores
+		//indicar quien empieza la partida
+
+	}
+
 }
