2ef6605cdb34e6ba8ea10e34b24419add1c386da
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-04-19 13:21:46 +0200

    Añadido Mesa.java

---
+++ src/es/uvigo/esei/aed1/core/Mesa.java
@@ -0,0 +1,22 @@
+/*
+* Representa la Mesa de juego. 
+* Estructura: se utilizar� un TAD adecuado. Piensa que hay 4 palos y de cada palo se pueden colocar cartas 
+* por cualquiera de los dos extremos (un array con 4 doblescolas parece lo m�s adecuado). La DobleCola se coment� en clase de teor�a
+* Funcionalidad: saber si es posible colocar una carta en la mesa, colocar una carta en la mesa, mostrar la mesa
+ */
+package es.uvigo.esei.aed1.core;
+
+public class Mesa {
+
+	//constructor
+	public Mesa() {
+
+	}
+
+	//a�adir m�s funcionalidades
+	// mostrar el estado de la mesa
+	@Override
+	public String toString() {
+	}
+
+}
